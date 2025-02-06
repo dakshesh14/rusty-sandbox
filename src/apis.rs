3fcdf4c9606a73bcc938a131afd22fcd91d550d0
Dakshesh Jain <dakshesh.jain14@gmail.com>
2025-02-06 23:10:20 +0530

    refactor: made functions to run python & cpp code more readable

--- src/apis.rs
+++ src/apis.rs
@@ -1,4 +1,7 @@
-use std::{fs, thread, time::Duration};
+use std::{
+    fs, thread,
+    time::{Duration, Instant},
+};
 
 use axum::{routing::post, Json, Router};
 use serde::{Deserialize, Serialize};
@@ -17,101 +20,91 @@ pub struct ExecutionResponse {
 }
 
 pub async fn run_python(Json(payload): Json<ExecutionRequest>) -> Json<ExecutionResponse> {
-    if let Some(sandbox) = Sandbox::new() {
-        let timeout = 15;
-        let start_time = std::time::Instant::now();
+    let timeout = Duration::from_secs(15);
+    let start_time = Instant::now();
 
-        let filename = format!("/tmp/{}.py", Uuid::new_v4());
+    let filename = format!("/tmp/{}.py", Uuid::new_v4());
 
-        // TODO: delete this file after execution
-        if let Err(e) = fs::write(&filename, &payload.code) {
+    let sandbox = match Sandbox::new() {
+        Some(s) => s,
+        None => {
             return Json(ExecutionResponse {
-                output: format!("Failed to write code to file: {}", e),
+                output: "Failed to create sandbox".to_string(),
             });
         }
+    };
 
-        let command = format!("python3 {}", filename);
-
-        while start_time.elapsed().as_secs() < timeout {
-            if sandbox.is_running() {
-                match sandbox.run_command(&command) {
-                    Ok(output) => {
-                        sandbox.terminate();
-                        return Json(ExecutionResponse { output });
-                    }
-                    Err(output) => {
-                        sandbox.terminate();
-                        return Json(ExecutionResponse { output });
-                    }
-                }
-            }
-            thread::sleep(Duration::from_secs(1));
-        }
-
-        sandbox.terminate();
+    if let Err(e) = fs::write(&filename, &payload.code) {
         return Json(ExecutionResponse {
-            output: "Execution timed out".to_string(),
+            output: format!("Failed to write code to file: {}", e),
         });
     }
 
+    let command = format!("python3 {}", &filename);
+
+    while start_time.elapsed() < timeout {
+        if sandbox.is_running() {
+            let output = match sandbox.run_command(&command) {
+                Ok(output) | Err(output) => output,
+            };
+            sandbox.terminate();
+            return Json(ExecutionResponse { output });
+        }
+        thread::sleep(Duration::from_secs(1));
+    }
+
+    sandbox.terminate();
     Json(ExecutionResponse {
-        output: "Failed to create sandbox".to_string(),
+        output: "Execution timed out".to_string(),
     })
 }
 
 pub async fn run_cpp(Json(payload): Json<ExecutionRequest>) -> Json<ExecutionResponse> {
-    if let Some(sandbox) = Sandbox::new() {
-        let timeout = 15;
-        let start_time = std::time::Instant::now();
+    let timeout = Duration::from_secs(15);
+    let start_time = Instant::now();
+    let id = Uuid::new_v4();
+    let source_file = format!("/tmp/{}.cpp", id);
+    let binary_file = format!("/tmp/{}", id);
 
-        let id = Uuid::new_v4();
-        let source_file = format!("/tmp/{}.cpp", id);
-        let binary_file = format!("/tmp/{}", id);
-
-        // TODO: delete this file after execution
-        if let Err(e) = fs::write(&source_file, &payload.code) {
+    let sandbox = match Sandbox::new() {
+        Some(s) => s,
+        None => {
             return Json(ExecutionResponse {
-                output: format!("Failed to write code to file: {}", e),
-            });
+                output: "Failed to create sandbox".to_string(),
+            })
         }
+    };
 
-        let compile_command = format!("g++ -o {} {}", binary_file, source_file);
-        match sandbox.run_command(&compile_command) {
-            Ok(_) => {
-                let run_command = format!("{}", binary_file);
-                while start_time.elapsed().as_secs() < timeout {
-                    if sandbox.is_running() {
-                        match sandbox.run_command(&run_command) {
-                            Ok(output) => {
-                                sandbox.terminate();
-                                return Json(ExecutionResponse { output });
-                            }
-                            Err(output) => {
-                                sandbox.terminate();
-                                return Json(ExecutionResponse { output });
-                            }
-                        }
-                    }
-                    thread::sleep(Duration::from_secs(1));
-                }
-            }
-            Err(output) => {
-                sandbox.terminate();
-                return Json(ExecutionResponse {
-                    output: format!("Compilation failed:\n{}", output),
-                });
-            }
-        }
-
-        sandbox.terminate();
-        Json(ExecutionResponse {
-            output: "Execution timed out".to_string(),
-        })
-    } else {
-        Json(ExecutionResponse {
-            output: "Failed to create sandbox".to_string(),
-        })
+    if let Err(e) = fs::write(&source_file, &payload.code) {
+        return Json(ExecutionResponse {
+            output: format!("Failed to write code to file: {}", e),
+        });
     }
+
+    let compile_command = format!("g++ -o {} {}", binary_file, source_file);
+    if let Err(output) = sandbox.run_command(&compile_command) {
+        sandbox.terminate();
+        return Json(ExecutionResponse {
+            output: format!("Compilation failed:\n{}", output),
+        });
+    }
+
+    let run_command = binary_file.clone();
+    while start_time.elapsed() < timeout {
+        if sandbox.is_running() {
+            let output = match sandbox.run_command(&run_command) {
+                Ok(output) | Err(output) => output,
+            };
+            sandbox.terminate();
+            return Json(ExecutionResponse { output });
+        }
+        thread::sleep(Duration::from_secs(1));
+    }
+
+    sandbox.terminate();
+    Json(ExecutionResponse {
+        output: "Execution timed out".to_string(),
+    })
 }
 
 pub fn get_routes() -> Router {
